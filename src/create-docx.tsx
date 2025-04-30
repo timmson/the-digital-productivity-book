b0f368f17608a1dd4fee6f7b207e9b8fb683ed77
timmson <timmson666@mail.ru>
2025-04-30 14:57:32 +0300

    Add docx generation

--- src/create-docx.tsx
+++ src/create-docx.tsx
@@ -1,28 +1,59 @@
-import {Document, Paragraph, HeadingLevel, TextRun} from "docx"
+import {lexer} from "marked"
+import {Document, Paragraph, HeadingLevel, TextRun, FileChild} from "docx"
 
-interface Structure {
+interface Config {
     title: string
     font: string
 }
 
-export const createDOCX = (structure: Structure) =>
+export const createDOCX = (config: Config, children: Array<FileChild>) =>
     new Document({
             sections: [
                 {
                     children: [
                         new Paragraph({
-                            heading: HeadingLevel.HEADING_1,
+                            heading: HeadingLevel.TITLE,
                             children: [
                                 new TextRun({
-                                    text: structure.title,
-                                    font: structure.font,
+                                    text: config.title,
+                                    font: config.font,
                                     size: "28pt",
-                                    bold: true
                                 })
                             ]
-                        })
+                        }),
+                        ...children
                     ]
                 }
             ]
         }
     )
+
+
+export const createChapter = (config: Config, structure: any): Array<Paragraph> => {
+    return structure.map((paragraph)  => {
+        switch (paragraph.type) {
+            case "heading":
+                return new Paragraph({
+                    pageBreakBefore: paragraph.depth == 1,
+                    heading: `Heading${paragraph.depth}`,
+                    children: [
+                        new TextRun({
+                            text: paragraph.text,
+                            font: config.font,
+                        })
+                    ]
+                })
+            default:
+                return new Paragraph({
+                    children: [
+                        new TextRun({
+                            text: paragraph.text,
+                            font: config.font,
+                            size: "12pt"
+                        })
+                    ]
+                })
+        }
+
+    })
+}
--- src/generate-docx.tsx
+++ src/generate-docx.tsx
@@ -1,19 +1,38 @@
 import * as fs from "fs"
-import {Packer} from "docx"
-import {createDOCX} from "./create-docx"
+import {Document, HeadingLevel, Packer, PageBreak, PageMargin, Paragraph, TextRun} from "docx"
+import {createChapter, createDOCX} from "./create-docx"
 
 import {lexer} from "marked"
 
-const structure = {
+let contents = fs.readFileSync("./docs/index.md", "utf-8");
+const index = lexer(contents)
+
+const config = {
     title: "Цифровая продуктивность",
-    font: "Times New Roman"
+    font: "Times New Roman",
+    index: index,
 }
-const index = lexer(fs.readFileSync("./docs/index.md", "utf-8"))
 
-console.log(JSON.stringify(index, null, 2))
+const extractHref = (element: any): Array<string> => {
+    if (element["href"])
+        return [element["href"]]
+    if (element["items"] && element["items"].length > 0)
+        return element["items"].map((item: any) => extractHref(item)).flat()
+    if (element["tokens"] && element["tokens"].length > 0)
+        return element["tokens"].map((token: any) => extractHref(token)).flat()
+    return []
+}
 
-// const docx = createDOCX(structure)
-//
-// Packer.toBuffer(docx).then((buffer) =>
-//     fs.writeFileSync("./docs/distr/the-productivity-book.docx", buffer)
-// )
+const files = index.map((item) => extractHref(item))
+    .flat().filter((file) => file !== "LICENSE")
+    .map((file) => fs.readFileSync(`./docs/${file}`, "utf-8"))
+
+const children = [contents, ...files]
+    .map((file) => createChapter(config, lexer(file)))
+    .flat()
+
+const docx = createDOCX(config, children)
+
+Packer.toBuffer(docx).then((buffer) =>
+    fs.writeFileSync("./docs/distr/the-productivity-book.docx", buffer)
+)
